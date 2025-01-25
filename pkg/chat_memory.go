f36a7e9a24d1fc52d1a80cabccd41ebb7077498a
Yiting Qiang <qiangyt@wxcount.com>
2025-01-25 17:31:42 +0800

    refactor chat memory

--- pkg/chat_memory.go
+++ pkg/chat_memory.go
@@ -1,49 +1,66 @@
 package batchai
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/openai/openai-go"
+	"github.com/qiangyt/batchai/comm"
 )
 
 type ChatMemoryT struct {
-	msgs []ChatMessage
+	msgs *comm.OrderedMap[ChatMessage]
 }
 
 type ChatMemory = *ChatMemoryT
 
 func NewChatMemory() ChatMemory {
 	return &ChatMemoryT{
-		msgs: []ChatMessage{},
+		msgs: comm.NewOrderedMap[ChatMessage](nil),
 	}
 }
 
 func (me ChatMemory) AddSystemMessage(content string) ChatMemory {
-	me.msgs = append(me.msgs, NewSystemMessage(content))
+	key := strconv.Itoa(me.msgs.Len())
+	return me.PutSystemMessage(key, content)
+}
+
+func (me ChatMemory) PutSystemMessage(key string, content string) ChatMemory {
+	me.msgs.Put(key, NewSystemMessage(content))
 	return me
 }
 
 func (me ChatMemory) AddAssistantMessage(content string) ChatMemory {
-	me.msgs = append(me.msgs, NewAssistantMessage(content))
+	key := strconv.Itoa(me.msgs.Len())
+	return me.PutAssistantMessage(key, content)
+}
+
+func (me ChatMemory) PutAssistantMessage(key string, content string) ChatMemory {
+	me.msgs.Put(key, NewAssistantMessage(content))
 	return me
 }
 
 func (me ChatMemory) AddUserMessage(content string) ChatMemory {
-	me.msgs = append(me.msgs, NewUserMessage(content))
+	key := strconv.Itoa(me.msgs.Len())
+	return me.PutUserMessage(key, content)
+}
+
+func (me ChatMemory) PutUserMessage(key string, content string) ChatMemory {
+	me.msgs.Put(key, NewUserMessage(content))
 	return me
 }
 
 func (me ChatMemory) ToChatCompletionMessageParamUnion() []openai.ChatCompletionMessageParamUnion {
-	r := make([]openai.ChatCompletionMessageParamUnion, len(me.msgs))
-	for i, msg := range me.msgs {
+	r := make([]openai.ChatCompletionMessageParamUnion, me.msgs.Len())
+	for i, msg := range me.msgs.Values() {
 		r[i] = msg.ToChatCompletionMessageParamUnion()
 	}
 	return r
 }
 
 func (me ChatMemory) Format() string {
-	r := make([]string, len(me.msgs))
-	for i, msg := range me.msgs {
+	r := make([]string, me.msgs.Len())
+	for i, msg := range me.msgs.Values() {
 		r[i] = msg.Format()
 	}
 	return strings.Join(r, "\n")
